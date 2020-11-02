47679f28c25d2557596f46f5017d9d70786123ea
widar <awidiarsana.aw@gmail.com>
2020-11-02 08:12:52 +0800

    merapikan file

---
+++ dashboard_tutor/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand font-weight-bold text-white" href="index.php">
+            <i class="fab fa-accusoft mr-2"></i>Tutorin Aja!
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item ml-2">
+                    <a class="nav-link text-white" href="#" role="button" data-toggle="modal"
+                        data-target="#profiltutor">PROFIL<i class="fas fa-user-alt ml-2"></i></a>
+                </li>
+
+                <li class="nav-item ml-2">
+                    <a class="nav-link text-white" href="#" role="button">LOGOUT<i
+                            class="fas fa-sign-out-alt ml-2"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
